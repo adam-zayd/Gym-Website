f52e5cdb81e81e3a07ee1169533efb7644282de0
Adam-Zayd Saeed <adam-zayd.saeed@student.manchester.ac.uk>
2025-02-10 14:08:14 +0000

    Added html page for exercises pulled from api - ExerciseDB. Added separate styling sheets and js scripts. Added checkbox filtering for exercises by muscle target group

--- exercises.js
+++ exercises.js
@@ -1,30 +1,70 @@
-document.addEventListener("DOMContentLoaded", function() {
+
+document.addEventListener("DOMContentLoaded", function(){
+    
     const exerciseList= document.getElementById("exerciseList");
+    const filtersContainer = document.getElementById("filters");
+    const muscleGroupsList = [
+        "abs", "biceps", "calves", "delts", "forearms", 
+        "glutes", "hamstrings", "lats", "pectorals", "quads", "traps", 
+        "triceps", "upper back"
+    ];
 
-    fetch("https://exercisedb.p.rapidapi.com/exercises?limit=20&offset=0", {
-        method: "GET",
-        headers:{
-            "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-            "x-rapidapi-key": "11c351d56emsh94e25460e1434e8p1df9d9jsn86b5275223e3"
-        }
-    })
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-
-        data.forEach(function(exercise){
-            let listItem= document.createElement("li");
-
-            listItem.innerHTML= `
-                <h3>${exercise.name}</h3>
-                <p>Target Muscle: ${exercise.target}</p>
-                <img src="${exercise.gifUrl}" >`;
-
-            exerciseList.appendChild(listItem);
+    function createFilters(){
+        muscleGroupsList.forEach(function(group){
+            const filterDiv= document.createElement("div");
+            const checkbox= document.createElement("input");
+            checkbox.type= "checkbox"; 
+            checkbox.id= group;  
+            checkbox.checked= false; 
+            checkbox.addEventListener('change', filterExercises);
+            const label= document.createElement("label");
+            label.setAttribute("for", group); 
+            label.textContent= group;
+            filterDiv.appendChild(checkbox);
+            filterDiv.appendChild(label);
+            filtersContainer.appendChild(filterDiv);
         });
-    })
-    .catch(function(error){
-        console.log("Error fetching exercises:", error);
-    });
+    }
+
+    function filterExercises(){
+        const checkedGroups= Array.from(document.querySelectorAll('input[type="checkbox"]:checked'))
+                                    .map(function(checkbox) {
+                                        return checkbox.id; 
+                                    });
+
+        if (checkedGroups.length== 0){
+            exerciseList.innerHTML= "<li>Please select at least one muscle group to see exercises.</li>";
+            return;
+        }
+
+        exerciseList.innerHTML= "";
+
+        checkedGroups.forEach(function(group){
+            fetchExercises(group);
+        });
+    }
+
+    function fetchExercises(muscleGroup){
+        fetch(`https://exercisedb.p.rapidapi.com/exercises/target/${muscleGroup}?limit=6&offset=0`, {
+            method: "GET",
+            headers:{
+                "x-rapidapi-host": "exercisedb.p.rapidapi.com",
+                "x-rapidapi-key": "11c351d56emsh94e25460e1434e8p1df9d9jsn86b5275223e3"
+            }
+        })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            data.forEach(function(exercise){ 
+                let listItem= document.createElement("li");
+                listItem.innerHTML= `
+                    <h3>${exercise.name}</h3>
+                    <img src="${exercise.gifUrl}">
+                `;
+                exerciseList.appendChild(listItem);
+            });
+        })
+    }
+    createFilters();
 });
